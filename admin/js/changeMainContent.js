385b80da52a2e62d31d5d544e056a32cb5586c68
ChiMinh <vochiminh177@gmail.com>
2024-10-22 17:13:43 +0700

    Update admin

---
+++ admin/js/changeMainContent.js
@@ -0,0 +1,24 @@
+function updateMainContent(mainContentKey) {
+    const mainContentMap = {
+        thongKe: `
+
+        `,
+        sanPham: `
+        
+        `,
+        donHang: `
+        
+        `,
+        khachHang: `
+        
+        `,
+        taiKhoan: `
+        
+        `
+    };
+    const mainContentDiv = document.getElementById("main-content");
+    if (mainContentMap[mainContentKey]) {
+
+        var 
+    }
+}
